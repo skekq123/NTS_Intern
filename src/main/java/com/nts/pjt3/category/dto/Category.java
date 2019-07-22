d886b86fd55efd466cef26b7473c0da8110721cb
고영빈 <youngbin.go@nts-corp.com>
2019-07-22 12:29:14 +0900

    카테고리 클래스 추가

---
+++ src/main/java/com/nts/pjt3/category/dto/Category.java
@@ -0,0 +1,18 @@
+package com.nts.pjt3.category.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * 
+ * DB 연결을 위한 Category 클래스
+ * 
+ * @author 고영빈
+ * 
+ */
+@Getter
+@Setter
+public class Category {
+	private long id;
+	private String name;
+}
