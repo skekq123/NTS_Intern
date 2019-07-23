c6fe170330660891e129fbbe95a2a034854ef778
고영빈 <youngbin.go@nts-corp.com>
2019-07-23 18:53:29 +0900

    DbConfig 작성

---
+++ src/main/java/com/nts/reservation/config/DbConfig.java
@@ -0,0 +1,36 @@
+package com.nts.reservation.config;
+
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * 
+ * DB 설정만 따로 담고 있는 클래스
+ * 
+ * @author 고영빈
+ * 
+ */
+
+@Configuration
+@EnableTransactionManagement
+public class DbConfig {
+
+	private final String DRIVER_CLASS_NAME= "com.mysql.jdbc.Driver";
+	private final String DB_URL = "jdbc:mysql://10.113.116.52:13306/user7";
+	private final String DB_USER = "user7";
+	private final String DB_PASSWORD = "user7";
+
+	@Bean
+	public DataSource dataSource() {
+		BasicDataSource dataSource = new BasicDataSource();
+		dataSource.setDriverClassName(DRIVER_CLASS_NAME);
+		dataSource.setUrl(DB_URL);
+		dataSource.setUsername(DB_USER);
+		dataSource.setPassword(DB_PASSWORD);
+		return dataSource;
+	}
+}
