ce9766c21d6b57ed709788a2c933dc0993733991
고영빈 <youngbin.go@nts-corp.com>
2019-07-24 14:29:18 +0900

    Refactor
    파라미터가 없는 메소드의 양식에 맞추어 변경

--- src/main/java/com/nts/reservation/category/dao/CategoryDao.java
+++ src/main/java/com/nts/reservation/category/dao/CategoryDao.java
@@ -2,28 +2,27 @@ package com.nts.reservation.category.dao;
 
 import static com.nts.reservation.category.dao.CategoryDaoSqls.SELECT_ALL;
 
-import java.util.Collections;
 import java.util.List;
 
 import javax.sql.DataSource;
 
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.nts.reservation.category.dto.Category;
 
 @Repository
 public class CategoryDao {
-	private NamedParameterJdbcTemplate jdbc;
+	private JdbcTemplate jdbc;
 	private RowMapper<Category> rowMapper = BeanPropertyRowMapper.newInstance(Category.class);
 
 	public CategoryDao(DataSource dataSource) {
-		this.jdbc = new NamedParameterJdbcTemplate(dataSource);
+		this.jdbc = new JdbcTemplate(dataSource);
 	}
 
 	public List<Category> selectAll() {
-		return jdbc.query(SELECT_ALL, Collections.EMPTY_MAP, rowMapper);
+		return jdbc.query(SELECT_ALL, rowMapper);
 	}
 }
