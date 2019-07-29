b179529b88b8ea578349128918fb70a60a6dbb7c
고영빈 <youngbin.go@nts-corp.com>
2019-07-29 14:56:13 +0900

    Promotion dto 추가

---
+++ src/main/java/com/nts/reservation/promotion/dto/Promotion.java
@@ -0,0 +1,12 @@
+package com.nts.reservation.promotion.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Promotion {
+	private Integer id;
+	private Integer productId;
+	private String productImageUrl;
+}
