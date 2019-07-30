77991fa1cdc5914a3bdc537e03a12ee792e61efb
고영빈 <youngbin.go@nts-corp.com>
2019-07-30 18:35:57 +0900

    PromotionService, Impl 추가

---
+++ src/main/java/com/nts/reservation/promotion/service/PromotionService.java
@@ -0,0 +1,9 @@
+package com.nts.reservation.promotion.service;
+
+import java.util.List;
+
+import com.nts.reservation.promotion.dto.Promotion;
+
+public interface PromotionService {
+	List<Promotion> getPromotions();
+}
