66a55b8743370d82b861f7349d84b5f2fa725267
고영빈 <youngbin.go@nts-corp.com>
2019-08-05 17:33:40 +0900

    DisplayInfo 쿼리문 추가

--- src/main/java/com/nts/reservation/displayInfo/dao/DisplayInfoSqls.java
+++ src/main/java/com/nts/reservation/displayInfo/dao/DisplayInfoSqls.java
@@ -8,4 +8,17 @@ public class DisplayInfoSqls {
 			+ "INNER JOIN file_info ON display_info_image.file_id = file_info.id "
 			+ "INNER JOIN display_info ON display_info.id = display_info_image.display_info_id "
 			+ "WHERE display_info.id = :displayInfoId;";
+	public static final String SELECT_DISPLAY_INFO = "SELECT category.id AS category_id, "
+			+ "category.name AS category_name, display_info.create_date AS create_date, "
+			+ "display_info.id AS display_info_id, display_info.email AS email, "
+			+ "display_info.homepage AS homepage, display_info.modify_date AS modify_date, "
+			+ "display_info.opening_hours AS opening_hours, display_info.place_lot AS place_lot, "
+			+ "display_info.place_name AS place_name, display_info.place_street AS place_street, "
+			+ "product.content AS product_content, product.description AS product_description, "
+			+ "product.event AS product_event, product.id AS product_id, "
+			+ "display_info.tel AS telephone "
+			+ "FROM display_info "
+			+ "INNER JOIN product ON product.id = display_info.product_id "
+			+ "INNER JOIN category ON category.id = product.category_id "
+			+ "WHERE display_info.id = :displayInfoId;";
 }
