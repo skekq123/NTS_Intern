8297ab5712012c338ab64a1278fa12f27d7dc6fb
고영빈 <youngbin.go@nts-corp.com>
2019-08-05 15:55:29 +0900

    상품 이미지 정보 쿼리문 추가

--- src/main/java/com/nts/reservation/product/dao/ProductDaoSqls.java
+++ src/main/java/com/nts/reservation/product/dao/ProductDaoSqls.java
@@ -25,4 +25,15 @@ public final class ProductDaoSqls {
 			+ "INNER JOIN product_image ON product.id = product_image.product_id and product_image.type = 'th' "
 			+ "INNER JOIN display_info ON product.id = display_info.product_id "
 			+ "WHERE product.category_id = :categoryId;";
+	public static final String SELECT_PROUDUCT_IMAGE = "SELECT file_info.content_type AS content_type, "
+			+ "file_info.create_date AS create_date, file_info.delete_flag AS delete_flag, "
+			+ "file_info.id AS file_info_id, file_info.file_name AS file_name, "
+			+ "file_info.modify_date AS modify_date, product_image.product_id AS product_id, "
+			+ "product_image.id AS product_image_id, file_info.save_file_name AS save_file_name, "
+			+ "product_image.type AS type "
+			+ "FROM product_image "
+			+ "INNER JOIN file_info ON file_info.id = product_image.file_id "
+			+ "INNER JOIN product ON product_image.product_id = product.id "
+			+ "INNER JOIN display_info ON display_info.product_id = product.id "
+			+ "WHERE display_info.id = :displayInfoId;";
 }
