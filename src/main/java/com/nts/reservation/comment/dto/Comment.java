32068709f6179e2901931fe967e7a9e7b83efbab
고영빈 <youngbin.go@nts-corp.com>
2019-08-08 12:28:30 +0900

    Refactor
    불필요한 필드 삭제

--- src/main/java/com/nts/reservation/comment/dto/Comment.java
+++ src/main/java/com/nts/reservation/comment/dto/Comment.java
@@ -11,12 +11,8 @@ public class Comment {
 	private String comment;
 	private int commentId;
 	private List<CommentImage> commentImages;
-	private String createDate;
-	private String modifyDate;
-	private int productId;
 	private String reservationDate;
 	private String reservationEmail;
-	private int reservationInfoId;
 	private String reservationName;
 	private String reservationTelephone;
 	private double score;
