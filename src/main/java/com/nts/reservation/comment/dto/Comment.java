622af5a492d5a249130e44fe4ae7110f5f64d43c
고영빈 <youngbin.go@nts-corp.com>
2019-08-12 18:33:48 +0900

    불필요한 reservation 변수 삭제

--- src/main/java/com/nts/reservation/comment/dto/Comment.java
+++ src/main/java/com/nts/reservation/comment/dto/Comment.java
@@ -11,9 +11,5 @@ public class Comment {
 	private String comment;
 	private int commentId;
 	private List<CommentImage> commentImages;
-	private String reservationDate;
-	private String reservationEmail;
-	private String reservationName;
-	private String reservationTelephone;
 	private double score;
 }
