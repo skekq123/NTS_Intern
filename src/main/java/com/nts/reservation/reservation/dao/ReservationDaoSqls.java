df4fa190635931fd2b73ee9447c31d4c3365e475
고영빈 <youngbin.go@nts-corp.com>
2019-08-21 16:41:02 +0900

    예약 갯수, 정보, 총계 sql 문 추가

--- src/main/java/com/nts/reservation/reservation/dao/ReservationDaoSqls.java
+++ src/main/java/com/nts/reservation/reservation/dao/ReservationDaoSqls.java
@@ -14,4 +14,25 @@ public class ReservationDaoSqls {
 		public static final String INSERT_RESERVE_PRICE = "INSERT INTO reservation_info_price( "
 			+ "reservation_info_id, product_price_id, count) "
 			+ "VALUES( :reservationInfoId, :productPriceId, :count);";
+		
+		public static final String SELECT_RESERVATION_SIZE = "SELECT COUNT(*) "
+			+ "FROM reservation_info "
+			+ "WHERE reservation_info.reservation_email = :reservationEmail;";
+		
+		public static final String SELECT_RESERVATION_INFO = "SELECT reservation_info.cancel_flag AS cancel_yn, "
+			+ "reservation_info.create_date AS create_date, reservation_info.display_info_id AS display_info_id, "
+			+ "reservation_info.modify_date AS modify_date, reservation_info.product_id AS product_id, "
+			+ "reservation_info.reservation_date AS reservation_date, reservation_info.reservation_email AS reservation_email, "
+			+ "reservation_info.id AS reservation_info_id, reservation_info.reservation_name AS reservation_name, "
+			+ "reservation_info.reservation_tel AS reservation_telephone "
+			+ "FROM reservation_info "
+			+ "WHERE reservation_info.reservation_email = :reservationEmail;";
+		
+		public static final String SELECT_TOTAL_PRICE = "SELECT SUM(reservation_info_price.count * product_price.price) AS a "
+			+ "FROM reservation_info_price "
+			+ "INNER JOIN reservation_info ON reservation_info.id = reservation_info_price.reservation_info_id "
+			+ "INNER JOIN product_price ON product_price.id = reservation_info_price.product_price_id "
+			+ "WHERE reservation_info.reservation_email = :reservationEmail "
+			+ "AND reservation_info.product_id = :productId "
+			+ "AND reservation_info.id = :reservationInfoId;";
 }
