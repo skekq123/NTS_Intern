94da03e105b7339e5bb7e7e8b850e9e8e093cc44
고영빈 <youngbin.go@nts-corp.com>
2019-08-21 16:42:36 +0900

    예약정보 리스트를 가져오는 dao 추가

--- src/main/java/com/nts/reservation/reservation/dao/ReservationDao.java
+++ src/main/java/com/nts/reservation/reservation/dao/ReservationDao.java
@@ -1,18 +1,28 @@
 package com.nts.reservation.reservation.dao;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.sql.DataSource;
 
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
+import com.nts.reservation.reservation.dto.ReservationInfo;
 import com.nts.reservation.reservation.dto.ReservationParam;
 
 @Repository
 public class ReservationDao {
 	private NamedParameterJdbcTemplate jdbc;
+	private RowMapper<ReservationInfo> rowMapperReservationInfo = BeanPropertyRowMapper
+			.newInstance(ReservationInfo.class);
 
 	public ReservationDao(DataSource dataSource) {
 		this.jdbc = new NamedParameterJdbcTemplate(dataSource);
@@ -37,5 +47,23 @@ public class ReservationDao {
 		params.addValue("reservationInfoId", reservationInfoId);
 		params.addValue("count", count);
 		return jdbc.update(ReservationDaoSqls.INSERT_RESERVE_PRICE, params);
+	}
+
+	public int getReservationSize(String reservationEmail) {
+		return jdbc.queryForObject(ReservationDaoSqls.SELECT_RESERVATION_SIZE,
+				Collections.singletonMap("reservationEmail", reservationEmail), Integer.class);
+	}
+
+	public List<ReservationInfo> getReservationInfos(String reservationEmail) {
+		return jdbc.query(ReservationDaoSqls.SELECT_RESERVATION_INFO,
+				Collections.singletonMap("reservationEmail", reservationEmail), rowMapperReservationInfo);
+	}
+
+	public int getTotalPrice(String reservationEmail, int productId, int reservationInfoId) {
+		Map<String, Object> params = new HashMap<>();
+		params.put("reservationEmail", reservationEmail);
+		params.put("productId", productId);
+		params.put("reservationInfoId", reservationInfoId);
+		return jdbc.queryForObject(ReservationDaoSqls.SELECT_TOTAL_PRICE, params, Integer.class);
 	}
 }
