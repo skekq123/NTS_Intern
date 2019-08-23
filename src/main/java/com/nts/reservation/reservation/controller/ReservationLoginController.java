763298edd159e35b3f39c09c43dfc17d40ea7d85
고영빈 <youngbin.go@nts-corp.com>
2019-08-23 18:57:10 +0900

    쿠키값 설정, myreservation 페이지 요청부분 추가

--- src/main/java/com/nts/reservation/reservation/controller/ReservationLoginController.java
+++ src/main/java/com/nts/reservation/reservation/controller/ReservationLoginController.java
@@ -1,8 +1,14 @@
 package com.nts.reservation.reservation.controller;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ReservationLoginController {
@@ -10,4 +16,28 @@ public class ReservationLoginController {
 	public String moveLoginPage() {
 		return "bookinglogin";
 	}
+	
+	private static final int COOKIE_TIME = 60;
+
+	@PostMapping(path = "/bookinglogin")
+	public String loginUser(@RequestParam("resrv_email") String reservationEmail,
+			@CookieValue(name = "userMail", required = false) Cookie userMail, HttpServletResponse response) {
+		if (userMail == null) {
+			userMail = new Cookie("userMail", reservationEmail);
+		} else {
+			userMail.setValue(reservationEmail);
+		}
+
+		userMail.setMaxAge(COOKIE_TIME);
+
+		response.addCookie(userMail);
+		
+		return "/myreservation";
+	}
+	@RequestMapping(value = "/myreservation", method = RequestMethod.GET)
+	public String moveReservationPage() {
+		return "myreservation";
+	}
+	
+
 }
