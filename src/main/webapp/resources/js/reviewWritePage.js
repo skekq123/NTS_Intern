3f128ca4ec5d5c17056f5dfb6c0b13f7a7c1a65c
고영빈 <youngbin.go@nts-corp.com>
2019-08-26 20:51:48 +0900

    이미지 첨부 기능, 양식체크, 제출  스크립트부분 추가

--- src/main/webapp/resources/js/reviewWritePage.js
+++ src/main/webapp/resources/js/reviewWritePage.js
@@ -41,6 +41,7 @@ function initRatingBox() {
             ratingList[index].fillStar();
         }
     });
+    initSubmitDisplay();
 }
 
 function initCommentFocus() {
@@ -57,6 +58,7 @@ function initCommentFocus() {
         if (targetText.value.length > 400) {
             alert('글자수는 ' + MAX_COMMENT_LENGTH + '개를 넘길 수 없습니다');
         }
+        initSubmitDisplay();
     });
 
     targetText.addEventListener('blur', () => {
@@ -65,11 +67,76 @@ function initCommentFocus() {
         }
     });
 }
+function initImageBox() {
+	let targetImageInput = document.querySelector("#reviewImageFileOpenInput");
+	let targetImageBox;
+	let targetImageOutput;
+	targetImageInput.addEventListener('change', evt => {
+        const image = evt.target.files[0];
+        targetImageBox = document.querySelector('.item');
+        targetImageOutput = document.querySelector('.item_thumb');
+        
+        targetImageBox.style.display = "block";
+        targetImageOutput.src = window.URL.createObjectURL(image);
+       
+        targetImageBox.style.width = targetImageOutput.width + 'px';
+  
+    })
 
+    let targetImageCancelBtn = document.querySelector('.spr_book');
+    targetImageCancelBtn.addEventListener('click', () => {
+    	targetImageInput.value = '';
+        targetImageBox.style.display = "none";
+        initSubmitDisplay();
+    })
+}
+function checkWriteForm() {
+	let targetScore = document.querySelector('.star_rank');
+	let targetText = document.querySelector('.review_textarea');
+    if (targetScore.innerText != 0 && targetText.value.length >= 5) {
+        return true;
+    }
+    return false;
+}
+function initSubmitDisplay() {
+	let targetSubmitBtn = document.querySelector('.bk_btn');
+    
+	if (checkWriteForm()) {
+		targetSubmitBtn.style.display = 'block';
+    } else {
+    	targetSubmitBtn.style.display = 'none'
+    }
+}
+function initSubmitBtn() {
+	let targetSubmitBtn = document.querySelector('.bk_btn');
+	let targetText = document.querySelector('.review_textarea');
+	let targetImageInput = document.querySelector("#reviewImageFileOpenInput");
+	let targetScore = document.querySelector('.star_rank');
+    targetSubmitBtn.addEventListener('click', function (evt) {
+
+        let writeForm = new FormData();
+        writeForm.append('comment', targetText.value.substr(0, MAX_COMMENT_LENGTH));
+        writeForm.append('score', parseInt(targetScore.innerText));
+        writeForm.append('productId', getUrlParameter('productId'));
+        writeForm.append('reservationInfoId', getUrlParameter('reservationInfoId'));
+
+        if (targetImageInput.files.length > 0) {
+            writeForm.append('imageFile', targetImageInput.files[0]);
+        }
+
+        requestPostAjax('/api/commentWrite', writeForm);
+        alert("리뷰가 등록되었습니다.");
+		location.href = "/reservation/myreservation";
+    });
+}
 document.addEventListener('DOMContentLoaded', function () {
     requestAjax(initDisplay, 'api/displayInfo/' + getUrlParameter('productId'));
     
     initRatingBox();
     
     initCommentFocus();
+    
+    initImageBox();
+    
+    initSubmitBtn();
 });
