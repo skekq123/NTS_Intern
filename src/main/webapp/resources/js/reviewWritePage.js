56ee29e654fb52dd20e5d6ba5b5e764482a98c4c
고영빈 <youngbin.go@nts-corp.com>
2019-08-26 12:10:12 +0900

    코멘트 입력부분 추가

--- src/main/webapp/resources/js/reviewWritePage.js
+++ src/main/webapp/resources/js/reviewWritePage.js
@@ -1,3 +1,5 @@
+const MAX_COMMENT_LENGTH = 400;
+
 function RatingObj(score) {
     this.score = score;
     this.starList = document.querySelectorAll('.rating_rdo');
@@ -41,8 +43,33 @@ function initRatingBox() {
     });
 }
 
+function initCommentFocus() {
+	let targetComment = document.querySelector('.review_write_info');
+	let targetText = document.querySelector('.review_textarea');
+	targetComment.addEventListener('click', () => {
+		targetComment.style.visibility = 'hidden';
+        targetText.focus();
+    });
+
+	let targetTextLength = document.querySelector('.guide_review');
+    targetText.addEventListener('keyup', e => {
+        targetTextLength.firstElementChild.innerText = targetText.value.length;
+        if (targetText.value.length > 400) {
+            alert('글자수는 ' + MAX_COMMENT_LENGTH + '개를 넘길 수 없습니다');
+        }
+    });
+
+    targetText.addEventListener('blur', () => {
+        if (targetText.value.length == 0) {
+            targetComment.style.visibility = 'visible';
+        }
+    });
+}
+
 document.addEventListener('DOMContentLoaded', function () {
     requestAjax(initDisplay, 'api/displayInfo/' + getUrlParameter('productId'));
     
     initRatingBox();
+    
+    initCommentFocus();
 });
