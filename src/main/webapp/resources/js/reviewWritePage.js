5ecf0f423efdb450009ad7a3bc0e42e8f36925d1
고영빈 <youngbin.go@nts-corp.com>
2019-08-26 11:55:33 +0900

    별점 클릭부분 구현

--- src/main/webapp/resources/js/reviewWritePage.js
+++ src/main/webapp/resources/js/reviewWritePage.js
@@ -1,3 +1,23 @@
+function RatingObj(score) {
+    this.score = score;
+    this.starList = document.querySelectorAll('.rating_rdo');
+}
+
+RatingObj.prototype = {
+	fillStar : function (){ 
+	    for (let i = 0; i < 5; ++i) {
+	        if (i <= this.score) {
+	            this.starList[i].checked = true;
+	        } else {
+	            this.starList[i].checked = false;
+	        }
+	    }
+	    let targetScore = document.querySelector('.star_rank');
+	    targetScore.classList.remove('gray_star');
+	    targetScore.innerText = this.score + 1;
+	}
+}
+
 function initDisplay(productInfo) {
 	
     let displayInfo = productInfo['displayInfo'];
@@ -5,7 +25,24 @@ function initDisplay(productInfo) {
     
     document.querySelector('.title').innerText = title;
 }
+function initRatingBox() {
+    let ratingList = new Object();
+    for (let i = 0; i < 5; ++i) {
+        ratingList[i] = new RatingObj(i);
+    }
+    let targetRatingBox = document.querySelector('.rating');
+    targetRatingBox.addEventListener('click', evt => {
+        let target = evt.target;
+
+        if (target.classList.contains('rating_rdo')) {
+            let index = target.value - 1;
+            ratingList[index].fillStar();
+        }
+    });
+}
 
 document.addEventListener('DOMContentLoaded', function () {
     requestAjax(initDisplay, 'api/displayInfo/' + getUrlParameter('productId'));
+    
+    initRatingBox();
 });
